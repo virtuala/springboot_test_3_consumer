eeb4e0a444321f7069f64adf98a56cdd49700176
virtuala <110410524@qq.com>
2019-03-08 13:51:13 +0800

    First

---
+++ src/main/java/com/example/springboot_test_3_consumer/SpringbootTest3ConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_test_3_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTest3ConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTest3ConsumerApplication.class, args);
+	}
+
+}
